f3192e40bb5d91c868b335949b4d2dab57a8146f
KevinGernigon <k.gernigon@rns.ecolescreatives.com>
2020-11-25 15:59:15 +0100

    memory_game

---
+++ memorygame.js
@@ -0,0 +1,29 @@
+// Write JavaScript here 
+var carte1 = document.getElementById("carte1");
+var carte2 = document.getElementById("carte2");
+var carte3 = document.getElementById("carte3");
+var carte4 = document.getElementById("carte4");
+var carte5 = document.getElementById("carte5");
+var carte6 = document.getElementById("carte6");
+var carte7 = document.getElementById("carte7");
+var carte8 = document.getElementById("carte8");
+
+/*function changehtml(){
+  carte1.innerHTML = "b";  
+}
+
+function clic (){
+  carte1.onclick = changehtml();
+}*/
+
+carte1.onclick = function(){
+  carte1.style.backgroundColor = "white";
+}
+  
+carte7.onclick = function(){
+  carte7.style.backgroundColor = "white";
+}
+  
+carte3.onclick = function(){
+  carte3.style.backgroundColor = "red";
+}
